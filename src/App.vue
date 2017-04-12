e973f7eab82f9bfabb68b8f7db0bd78db408b6dd
itbainianmei.github.com <13464587379@163.com>
2017-04-12 18:05:11 +0800

    实现完毕

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,88 @@
 <template>
   <div id="app">
-     <ul id="demo">
+    <ul id="demo">
       <item
-        class="item"
-        :model="data">
+        class="item" v-for="model of data"
+        :model="model"
+        >
       </item>
     </ul>
-
   </div>
 </template>
-
 <script>
 import Item from './components/item.vue'
 export default {
   name: 'app',
   data (){
     return {
-      data:{
-        name: 'My Tree',
-        isSelect: false,
-        children: [
-          { name: 'hello',isSelect: true },
-          { name: 'wat',isSelect: false },
-          {
-            name: 'child folder',
-            isSelect: false,
-            children: [
-              {
-                name: 'child folder777',
-                isSelect: false,
-                children: [
-                  { name: 'hello' ,isSelect: false},
-                  { name: 'wat' ,isSelect: false}
-                ]
-              },
-              { name: 'hello' ,isSelect: false},
-              { name: 'wat',isSelect: false },
-              {
-                name: 'child folder666',
-                isSelect: false,
-                children: [
-                  { name: 'hello',isSelect: false },
-                  { name: 'wat',isSelect: false }
-                ]
-              }
-            ]
-          }
-        ]
-      }
+      data:[
+        {
+          name: 'My Tree-one',
+          isSelect: false,
+          children: [
+            { name: 'hello',isSelect: true },
+            { name: 'wat',isSelect: false },
+            {
+              name: 'child folder',
+              isSelect: false,
+              children: [
+                {
+                  name: 'child folder777',
+                  isSelect: false,
+                  children: [
+                    { name: 'hello' ,isSelect: false},
+                    { name: 'wat' ,isSelect: false}
+                  ]
+                },
+                { name: 'hello' ,isSelect: false},
+                { name: 'wat',isSelect: false },
+                {
+                  name: 'child folder666',
+                  isSelect: false,
+                  children: [
+                    { name: 'hello',isSelect: false },
+                    { name: 'wat',isSelect: false }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'My Tree-two',
+          deep:1,
+          isSelect: false,
+          children: [
+            { name: 'hello',isSelect: true },
+            { name: 'wat',isSelect: false },
+            {
+              name: 'child folder',
+              isSelect: false,
+              children: [
+                {
+                  name: 'child folder777',
+                  isSelect: false,
+                  children: [
+                    { name: 'hello' ,isSelect: false},
+                    { name: 'wat' ,isSelect: false}
+                  ]
+                },
+                { name: 'hello' ,isSelect: false},
+                { name: 'wat',isSelect: false },
+                {
+                  name: 'child folder666',
+                  isSelect: false,
+                  children: [
+                    { name: 'hello',isSelect: false },
+                    { name: 'wat',isSelect: false }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
-  },
-  watch :{
-    data: {
-      handler:function(val,oldVal){
-        console.log(oldVal)
-      },
-      deep:true
-    }
-  },
-  methods:{
-     
   },
   components:{
     Item
